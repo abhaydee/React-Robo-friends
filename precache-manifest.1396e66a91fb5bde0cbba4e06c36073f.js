dfc404b4e8b67d5cf4e7ab246d6a81e4f421c795
abhaydee <abhaygirish1997.com>
2019-11-10 15:20:34 +0530

    Updates

---
+++ precache-manifest.1396e66a91fb5bde0cbba4e06c36073f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/React-Robo-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "9ac4a335ae9e4cf34e9e",
+    "url": "/React-Robo-friends/static/js/runtime~main.9ac4a335.js"
+  },
+  {
+    "revision": "768e50d10cda2b7a950f",
+    "url": "/React-Robo-friends/static/js/main.768e50d1.chunk.js"
+  },
+  {
+    "revision": "960d7acd4f0e36af18f3",
+    "url": "/React-Robo-friends/static/js/1.960d7acd.chunk.js"
+  },
+  {
+    "revision": "768e50d10cda2b7a950f",
+    "url": "/React-Robo-friends/static/css/main.25884f34.chunk.css"
+  },
+  {
+    "revision": "960d7acd4f0e36af18f3",
+    "url": "/React-Robo-friends/static/css/1.4aca5b96.chunk.css"
+  },
+  {
+    "revision": "56d5ee33bc9c8628e67f44856fa55481",
+    "url": "/React-Robo-friends/index.html"
+  }
+];
